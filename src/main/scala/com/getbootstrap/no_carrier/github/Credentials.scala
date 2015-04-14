ee6ac4178ccb0c7655479179b2ed181d13d5c597
Chris Rebert <code@rebertia.com>
2015-04-13 20:03:28 -0700

    add UserAgentWire

--- src/main/scala/com/getbootstrap/no_carrier/github/Credentials.scala
+++ src/main/scala/com/getbootstrap/no_carrier/github/Credentials.scala
@@ -6,4 +6,5 @@ import com.jcabi.http.wire.RetryWire
 case class Credentials(username: String, password: String) {
   private def basicGithub: Github = new RtGithub(username, password)
   def github: Github = new RtGithub(basicGithub.entry.through(classOf[RetryWire])) // FIXME: use RetryCarefulWire once it's available
+  // FIXME: also make use of UserAgentWire
 }
