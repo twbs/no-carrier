015c219fef20b7bc941d8fa2981d1a39732ae7a4
Chris Rebert <code@rebertia.com>
2015-04-13 20:02:58 -0700

    An issue's description doesn't count as a comment

--- src/main/scala/com/getbootstrap/no_carrier/github/FancyIssue.scala
+++ src/main/scala/com/getbootstrap/no_carrier/github/FancyIssue.scala
@@ -8,9 +8,12 @@ import InstantOrdering._
 
 class FancyIssue(val issue: Issue, val label: String, val timeout: Duration)(implicit clock: Clock) {
   lazy val lastLabelledAt: Instant = issue.lastLabelledWithAt(label).get
-  lazy val lastCommentedOnAt: Instant = issue.smartComments.map{ _.createdAt.toInstant }.max
+  lazy val lastCommentedOnAt: Option[Instant] = issue.smartComments.map{ _.createdAt.toInstant }.maxOption
   lazy val lastClosedAt: Option[Instant] = issue.smartEvents.filter{ _.isClosed }.map{ _.createdAt.toInstant }.maxOption
-  lazy val hasSubsequentComment: Boolean = lastLabelledAt < lastCommentedOnAt
+  lazy val hasSubsequentComment: Boolean = lastCommentedOnAt match {
+    case None => false
+    case Some(commentedAt) => lastLabelledAt < commentedAt
+  }
   lazy val wasClosedAfterLabelling: Boolean = lastClosedAt match {
     case None => false
     case Some(closedAt) => lastLabelledAt < closedAt
