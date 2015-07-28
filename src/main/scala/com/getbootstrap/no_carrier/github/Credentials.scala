7ee02ba6fc796c54cc02e47ba7bb3863b4d2494a
Chris Rebert <code@rebertia.com>
2015-07-27 23:50:55 -0700

    Credentials.toString: Don't leak the password

--- src/main/scala/com/getbootstrap/no_carrier/github/Credentials.scala
+++ src/main/scala/com/getbootstrap/no_carrier/github/Credentials.scala
@@ -6,4 +6,5 @@ import com.getbootstrap.no_carrier.http.{UserAgent, SuperWire}
 case class Credentials(username: String, password: String) {
   private def basicGithub: Github = new RtGithub(username, password)
   def github(threshold: Int)(implicit userAgent: UserAgent): Github = new RtGithub(basicGithub.entry.through(classOf[SuperWire], userAgent, new Integer(threshold)))
+  override def toString: String = s"Credentials(username = ${username}, password = <SECRET>)"
 }
