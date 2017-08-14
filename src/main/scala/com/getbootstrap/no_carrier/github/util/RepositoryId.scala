fedda2c9ddee2f78dbd588bd7fbf4e99ccb7464e
Radon Rosborough <radon.neon@gmail.com>
2017-08-13 17:43:21 -0700

    Allow . to be used in repository names (#23)
    This fixes a bug where repositories with periods in their names are
    not recognized by NO CARRIER, even though they are valid.

--- src/main/scala/com/getbootstrap/no_carrier/github/util/RepositoryId.scala
+++ src/main/scala/com/getbootstrap/no_carrier/github/util/RepositoryId.scala
@@ -3,7 +3,7 @@ package com.getbootstrap.no_carrier.github.util
 import com.jcabi.github.Coordinates.{Simple=>RepoId}
 
 object RepositoryId {
-  private val OwnerSlashRepo = "([a-zA-Z0-9_-]+)/([a-zA-Z0-9_-]+)".r
+  private val OwnerSlashRepo = "([a-zA-Z0-9_-]+)/([a-zA-Z0-9._-]+)".r
 
   def unapply(ownerRepo: String): Option[RepoId] = {
     ownerRepo match {
